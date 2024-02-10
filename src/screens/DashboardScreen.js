5e98f31dee4230073c401a1b2fa7be6c93d51bf9
lfaitanin <lfaitanin@outlook.com>
2024-02-09 21:53:16 -0300

    menu

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -8,37 +8,6 @@ const DashboardScreen = ({navigation}) => {
         <Text>Seu Nome</Text>
         <Text>Condomínio - Bloco - Unidade</Text>
       </HStack>
-      <Box padder>
-        <Card>
-          <CardItem header button onPress={() => alert("Comunicados")}>
-            <Text>Comunicados</Text>
-          </CardItem>
-        </Card>
-        <Card>
-          <CardItem button onPress={() => alert("Encomendas")}>
-            <Body>
-              <Icon name="cube" />
-              <Text>Encomendas</Text>
-            </Body>
-          </CardItem>
-        </Card>
-        <Card>
-          <CardItem button onPress={() => alert("Boletos")}>
-            <Body>
-              <Icon name="card" />
-              <Text>Boletos</Text>
-            </Body>
-          </CardItem>
-        </Card>
-        <Card>
-          <CardItem button onPress={() => alert("Serviços")}>
-            <Body>
-              <Icon name="hammer" />
-              <Text>Serviços</Text>
-            </Body>
-          </CardItem>
-        </Card>
-      </Box>
       <HStack>
         <Button iconLeft vertical>
           <Icon name="apps" type="MaterialIcons" />
