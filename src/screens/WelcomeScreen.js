a7e58fcf555fc90cba6ed5f0a63315a91ee0a716
hbschmidt <henriquebelz@gmail.com>
2024-02-25 19:05:19 -0300

    usando token para notificação

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,63 +1,12 @@
 import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
-import Constants from 'expo-constants';
-import * as Notifications from 'expo-notifications';
-import { PermissionsAndroid } from 'react-native';
-import { createClient } from '@segment/analytics-react-native';
-import messaging from '@react-native-firebase/messaging';
-import { DeviceTokenPlugin } from '@segment/analytics-react-native-plugin-device-token';
+
 
 
 const WelcomeScreen = ({ navigation }) => {
   
   useEffect(() => {
-    if (__DEV__ && Constants.expoConfig ) {
-      registerForPushNotificationsExpoAsync();
-    } else {
-      registerForPushNotificationsFirebaseAsync();
-    }
-
   }, []);
-
-  async function registerForPushNotificationsExpoAsync() {
-    let token;
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      console.log('Failed to get push token for push notification!');
-      return;
-    }
-
-    console.log("vamos pegar o token? " + Constants.expoConfig.name)
-    //token = (await Notifications.getExpoPushTokenAsync()).data;
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log('Expo Push Token:', token);
-
-    // Aqui você pode enviar o token para seu servidor se necessário
-  }
-
-  async function registerForPushNotificationsFirebaseAsync() {
-    //PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
-    // Certifique-se de que você tenha solicitado as permissões necessárias no Android manifest ou Info.plist para iOS.
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-    if (enabled) {
-      console.log('Authorization status:', authStatus);
-      const token = await messaging().getToken();
-      console.log('Firebase Push Token:', token);
-      
-      // Aqui você pode enviar o token para seu servidor se necessário
-    }
-  }
-
 
   return (
     <View style={styles.container}>
